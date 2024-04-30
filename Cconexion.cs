28fc8f29250b8b9d0e18bee92c23a08eeb903242
juanma161008 <168549535+juanma161008@users.noreply.github.com>
2024-04-30 08:20:41 -0500

    Add files via upload

---
+++ Cconexion.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace skyedge.clases
+{
+    class Cconexion
+    {
+        //Se define la ruta de la base de datos
+        static private string CadenaConexion = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\JUAN MANUEL CARDENAS\Documents\POB\skyedge\skyedge\dbskyedge.mdf;Integrated Security=True;Connect Timeout=30";
+
+        //Definir una variable para cargar la base de datos
+        private SqlConnection Conexion = new SqlConnection(CadenaConexion);
+
+        //metodo para abrir la base de datos
+        public SqlConnection AbrirConexion()
+        {
+            if (Conexion.State == ConnectionState.Closed)
+                Conexion.Open();
+            return Conexion;
+        }
+
+
+
+        //método para cerrar la base de datos
+        public SqlConnection CerrarConexion()
+        {
+            if (Conexion.State == ConnectionState.Open)
+                Conexion.Close();
+            return Conexion;
+        }
+    }
+}
